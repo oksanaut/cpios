6d286944aae0b43a0b904c7ef58da39dc790397c
Oksana Timonin <one@oneapart.com>
2014-10-07 16:58:50 -0700

    Tip Calculator exercise

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Oksana Timonin on 07/10/2014.
+//  Copyright (c) 2014 oksanaut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
